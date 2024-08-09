f46ee6a8f39ed53582bd5fb256cb02684a9a0894
satyajitPatel <thriver1988@gmail.com>
2024-08-09 11:52:06 +0530

    add middleware

--- server.js
+++ server.js
@@ -19,20 +19,32 @@ app.get("/users", (req, res) => {
     `;
     res.send(html); // returns and continues with the funtion
 });
+
+// decode | middlewarw | plugin
+app.use(express.json());
 // REST API
-app.get("/api/users/:id", (req, res) => { // :something means any dynamic id is coming
+app.post("/api/users", (req, res) => {
+    // return res.json({"status": "pending"});
+    const body = req.body; // body = undefined, if we don't decode(middleware)
+    console.log(body);
+    return res.json(body);
+});
+app.get("/api/users", (req, res) => {
+    // result = users.find({}); // find all
+    // return res.json(result);
+    return res.json(users);
+});
+app.route("/api/users/:id")
+.get((req, res) => { // :something means any dynamic id is coming
     const id = Number(req.params.id);
     const user = users.find((user) => user.id == id);
     return res.json(user); // returns and exist the function
-});
-app.post("/api/users", (req, res) => {
-    return res.json({status: "pending"});
-});
-app.patch("/api/users/:id", (req, res) => {
-    return res.json({status: "pending"});
-});
-app.delete("/api/users/:id", (req, res) => {
-    return res.json({status: "pending"});
+})
+.patch((req, res) => {
+    return res.json({"status": "pending"});
+})
+.delete((req, res) => {
+    return res.json({"status": "pending"});
 });
 
 
